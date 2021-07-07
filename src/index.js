1f2e7a455b2c39ba939f529c5b3fe9f37d2d5c79
Sergiy Yuschenko <eester33@gmail.com>
2021-07-07 19:41:36 +0300

    Додає стартові файли проекту плюс створення галереї з допомогою handlebars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './sass/main.scss';
+import menuCards from './templates/menu-cards.hbs'
+import menus from './menu.json';
+
+const menuListEl = document.querySelector('.js-menu');
+
+const menuCardsEl = menuCards(menus);
+
+menuListEl.insertAdjacentHTML('beforeend', menuCardsEl);
+
+console.log(menuCards(menus));
+
+
